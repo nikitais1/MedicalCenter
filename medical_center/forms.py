409fa475040f8803bb19644b290fea2ba12ba09c
Nikita <nikita.isaev2001@mail.ru>
2024-06-01 23:14:59 +0300

    initial

--- medical_center/forms.py
+++ medical_center/forms.py
@@ -2,37 +2,33 @@ from django import forms
 from medical_center.models import Service, Category, Cart
 
 
-class CategoryForm(forms.ModelForm):
+class StyleFormMixin:
+    """Форма со стилями"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class CategoryForm(StyleFormMixin, forms.ModelForm):
     """Форма создания категорий медицинских услуг."""
+
     class Meta:
         model = Category
         fields = ("category_title", "category_description", "category_image",)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
 
-
-class ServicesForm(forms.ModelForm):
+class ServicesForm(StyleFormMixin, forms.ModelForm):
     """Форма создания услуги."""
+
     class Meta:
         model = Service
         fields = ("services_title", "services_description", "category", "price", "deadline",)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
 
-
-class CartForm(forms.ModelForm):
+class CartForm(StyleFormMixin, forms.ModelForm):
     """Форма создания корзины."""
+
     class Meta:
         model = Cart
         fields = ("services", "date", "client",)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
